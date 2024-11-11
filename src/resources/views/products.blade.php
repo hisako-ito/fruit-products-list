71ffc94d5887aab6432f5f9668ec89ff81769c59
hisako_ito <hisahisaito@gmail.com>
2024-11-12 03:02:22 +0900

    model,seeder,migrationファイル追加

--- src/resources/views/products.blade.php
+++ src/resources/views/products.blade.php
@@ -10,12 +10,12 @@
             <h2>商品一覧</h2>
             <form class="add-form" action="/products/register" method="get">
             @csrf
-                <button class="add-form__btn">+ 商品を追加</button>
+                <input class="add-form__btn" type="submit" value="+ 商品を追加">
             </form>
         </div>
 
-        <div class="search-form">
-            <form action="/products/search" method="get">
+        <div class="products-list__sidebar">
+            <form class="search-sort-form" action="/products/search" method="get">
             @csrf
                 <input class="search-form__keyword-input" type="text" name="keyword" placeholder="商品名で検索" value="{{request('keyword')}}">
                 <div class="search-form__btn">
@@ -23,16 +23,35 @@
                 </div>
                 <div class="sort-form">
                     <h3>価格順で表示</h3>
+                    <select class="search-form__sort-select" name="category_id" id="">
+                        <option disabled selected>価格で並べ替え</option>
+                        <option value="">高い順に表示</option>
+                        <option value="">低い順に表示</option>
+                    </select>
                 </div>
             </form>
         </div>
 
-        <div class="box3">box3</div>
-        <div class="box4">box4</div>
-        <div class="box5">box5</div>
-        <div class="box6">box6</div>
-        <div class="box7">box7</div>
-        <div class="box8">box8</div>
-        <div class="box9">box9</div>
-</div>
+        <div class="products-list__content">
+            <div class="products-list__content-card">
+                <div class="card__img">
+                    <img src="" alt="" />
+                </div>
+                <div class="card__content">
+                    <span class="card__name">商品名</span>
+                    <span class="card__price">価格</span>
+                </div>
+            </div>
+            <div class="products-list__content-card">box4</div>
+            <div class="products-list__content-card">box5</div>
+            <div class="products-list__content-card">box6</div>
+            <div class="products-list__content-card">box7</div>
+            <div class="products-list__content-card">box8</div>
+        </div>
+
+        <div class="products-list__pagination">
+            <div class="box9">box9</div>
+        </div>
+
+    </div>
 @endsection
